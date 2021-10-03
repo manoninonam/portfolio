331394cd5f0e0aa164477c1cde930bb71d28c7ee
manoninonam <manon.heijmans@gmail.com>
2021-10-03 14:37:25 +0200

    updated responsiveness

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,24 @@
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
+    disableScroll();
   }
   
   function closeNav() {
     document.getElementById("myNav").style.width = "0%";
+    enableScroll();
   }
+
+  function disableScroll() {
+   
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+  
+ 
+        window.onscroll = function() {
+            window.scrollTo(scrollLeft, scrollTop);
+        };
+}
+  
+function enableScroll() {
+    window.onscroll = function() {};
+}
